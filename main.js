07551ab242207a0f5df349183dc5d55ed4e37c5c
saeed <mvvva001@gmail.com>
2023-05-09 05:56:43 +0500

    clo'ck js code

--- main.js
+++ main.js
@@ -68,3 +68,39 @@ todoList.addEventListener('click', event => {
         deleteTodo(index);
     }
 });
+
+
+// CLO`CK
+function updateClock() {
+    var now = new Date();
+    var year = now.getFullYear();
+    var month = now.getMonth() + 1;
+    var day = now.getDate();
+    var hour = now.getHours();
+    var minute = now.getMinutes();
+    var second = now.getSeconds();
+    
+    if (month < 10) {
+      month = "0" + month;
+    }
+    if (day < 10) {
+      day = "0" + day;
+    }
+    if (hour < 10) {
+      hour = "0" + hour;
+    }
+    if (minute < 10) {
+      minute = "0" + minute;
+    }
+    if (second < 10) {
+      second = "0" + second;
+    }
+    
+    var date = day + "-" + month + "-" + year;
+    var time = hour + ":" + minute + ":" + second;
+    
+    document.getElementById("date").textContent = date;
+    document.getElementById("time").textContent = time;
+    }
+    
+    setInterval(updateClock, 1000);
